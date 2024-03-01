64eb7c17724c4d38f7ee7901a9c9bb567f2b7911
Kunalingle307 <130652340+Kunalingle307@users.noreply.github.com>
2024-03-01 14:35:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def index_post():
     #endpoint = os.environ['ENDPOINT']
     #location = os.environ['LOCATION']
 
-    key="94a5ad7bbedd4e6f833ec571c6d4b8a3"
+    key="2ffb839268a94db5a352e99011f4dc60"
     endpoint="https://api.cognitive.microsofttranslator.com/"
     location="eastus"
 
